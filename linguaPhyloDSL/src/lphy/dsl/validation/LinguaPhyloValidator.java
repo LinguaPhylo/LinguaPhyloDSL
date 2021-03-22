d0ce45af87652fb19e06c08c325109d239c0d72c
alexeid <alexei.drummond@gmail.com>
2021-03-22 13:06:49 +1300

    validation of arguments for LPhyDistribution calls

--- linguaPhyloDSL/src/lphy/dsl/validation/LinguaPhyloValidator.java
+++ linguaPhyloDSL/src/lphy/dsl/validation/LinguaPhyloValidator.java
@@ -3,6 +3,17 @@
  */
 package lphy.dsl.validation;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.xtext.validation.Check;
+
+import lphy.dsl.linguaPhylo.Argument;
+import lphy.dsl.linguaPhylo.InstantiatedDistribution;
+import lphy.dsl.linguaPhylo.LPhyDist;
+import lphy.dsl.linguaPhylo.LPhyModel;
+import lphy.dsl.linguaPhylo.LinguaPhyloPackage;
 
 /**
  * This class contains custom validation rules. 
@@ -21,5 +32,38 @@ public class LinguaPhyloValidator extends AbstractLinguaPhyloValidator {
 //					INVALID_NAME);
 //		}
 //	}
+	
+	
+	@Check
+	public void checkInstantiatedDistribution(InstantiatedDistribution distribution) {
+		
+		var arguments = distribution.getArguments();
+		
+		var typeSpec = distribution.getTypeSpec();
+		
+		if (typeSpec instanceof LPhyDist) {
+			LPhyDist lphyDist = (LPhyDist)typeSpec;
+			
+			LPhyModel model = lphyDist.getDistributionType();
+				
+			var parameters = model.getParameters();
+			
+			for (Argument argument : arguments) {
+				if (!parameters.contains(argument.getName()) && !argument.getName().equals("replicates")) {
+					error("LPhyModel " + model.getName() + " doesn't have a parameter named " + argument.getName(), argument, LinguaPhyloPackage.Literals.ARGUMENT__NAME);
+				}
+			}
+			
+			List<String> argumentNames = arguments.stream().map(x -> x.getName()).collect(Collectors.toList());
+			
+			for (String parameter : parameters) {
+				if (!argumentNames.contains(parameter)) {
+					error("LPhyModel " + model.getName() + " expecting a parameter named " + parameter, lphyDist, LinguaPhyloPackage.Literals.LPHY_DIST__DISTRIBUTION_TYPE);
+	
+				}
+			}
+		}
+		
+	}
 	
 }
